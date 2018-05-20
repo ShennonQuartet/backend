57051aaa9ddfe23899dd05f0c91d03cca9c63a9f
Boris Tseytlin <b.tseytlin@start.film>
2018-05-20 11:59:13 +0300

    allow incidents without users

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -19,18 +19,20 @@ class UserRelatedField(serializers.RelatedField):
         return serializer.data
 
     def to_internal_value(self, data):
+        if not data:
+            return None
         serializer = WriteUserSerializer(data=data)
         if serializer.is_valid():
             user = User.objects.filter(**serializer.validated_data).first()
             if not user:
-                raise serializers.ValidationError("User should exist")
+                return None
             return user
         else:
             raise(serializer.errors)
 
 
 class IncidentSerializer(serializers.ModelSerializer):
-    user = UserRelatedField(queryset=User.objects, required=True)
+    user = UserRelatedField(queryset=User.objects, allow_null=True)
 
     class Meta:
         model = Incident
